3ba8bde757087157005579af644104c14fea18ed
Marc Anguera Insa <srmarc.ai@gmail.com>
2025-07-18 00:53:16 +0200

    bump to v0.20.0 🚀

--- lib/tonic.rb
+++ lib/tonic.rb
@@ -7,7 +7,7 @@ require_relative "tonic/helpers"
 require_relative "tonic/filters"
 
 module Tonic
-  VERSION = "0.19.0"
+  VERSION = "0.20.0"
   REPO = "https://github.com/Subgin/tonic"
   MAGIC_ATTRS = %w(name description images category tags id dom_id detail_page_link)
   SKIP_FOR_FILTERS = MAGIC_ATTRS - %w(category tags)
